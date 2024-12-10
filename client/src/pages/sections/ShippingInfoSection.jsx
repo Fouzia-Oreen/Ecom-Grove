bff6929ce00cb176e10f689585c58ef1d12aab48
Fouzia-Oreen <fouziaoreen236@gmail.com>
2024-12-10 23:58:18 +0600

    shipping info added

---
+++ client/src/pages/sections/ShippingInfoSection.jsx
@@ -0,0 +1,19 @@
+import { shippingInfo } from '../../assets/data'
+
+const ShippingInfoSection = () => {
+  return (
+    <div className='container flex flex-col md:flex-row items-center justify-evenly pt-12'>
+      {
+        shippingInfo.map((info) => (
+          <div key={info.id} className='flex flex-col gap-2 items-center mb-4'>
+            <img src={info.url} alt="" className='h-12'/>
+            <h3 className='text-lg font-medium'>{info.name}</h3>
+            <p className='text-neutral-500'>{info.desc}</p>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default ShippingInfoSection
