f2e780602f1a1d9c12468d4f420dd9c2f26ac244
Fouzia-Oreen <fouziaoreen236@gmail.com>
2025-01-28 00:39:36 +0600

    debugged

--- api/server.js
+++ api/server.js
@@ -2,14 +2,15 @@
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import express from 'express';
-import multer from "multer";
+import path from "path";
 import categoryRoutes from './routes/categoryRoutes.js';
 import productRoutes from "./routes/productRoutes.js";
 import uploadRoutes from "./routes/uploadRoutes.js";
 import userRoutes from './routes/userRoutes.js';
 import brandRoutes from './routes/brandRoutes.js';
 import subCategoryRoutes from './routes/subCategoryRoutes.js';
-
+import  orderRoutes from './routes/orderRoutes.js';
+import  headerRoutes from './routes/headerRoutes.js';
 
 dotenv.config();
 
@@ -31,24 +32,17 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/brand", brandRoutes);
 app.use("/api/subcategory", subCategoryRoutes);
-
 app.use("/api/upload", uploadRoutes);
+app.use("/api/orders", orderRoutes );
+app.use("/api/headers", headerRoutes );
 
-
-
-// default error handler for multer and others
-app.use((err, req, res, next) => {
-    if (err ) {
-        if (err instanceof multer.MulterError) {
-            res.status(500).send("There was an error")
-        } else {
-            res.status(500).send(err.message)
-        }   
-    }       res.status(200).send({
-    message: "Image uploaded successfully",
+app.get("/api/config/paypal", (req, res) => {
+    res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
-next()
-})
+
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname + "/uploads")));
+
 
 // listening - port
 app.listen(PORT, () => {
